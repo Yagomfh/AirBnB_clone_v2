e7e9e33e8019e65a390df70d824b4f3420e91069
Arthur <arthmayer@outlook.fr>
2021-03-29 12:22:17 +0000

    add q7 update user class

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy import Column, String,
+from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 
 
