e0549aa88f1b9fd2beab5c02745ccd68a5a1363e
yagomfh <yagomfh@gmail.com>
2021-03-29 07:03:35 +0000

    feat: initial solution

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review}
 
 
 class FileStorage:
@@ -8,9 +20,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        allObj = FileStorage.__objects
+        if cls is None:
+            return allObj
+        res = {}
+        for obj in allObj:
+            if str(cls) == str(type(allObj[obj])):
+                res[obj] = allObj[obj]
+        return res
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -27,19 +46,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -48,3 +54,12 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Method to delete an object"""
+        allObj = FileStorage.__objects
+        if obj is None:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        allObj.pop(key)
+        return
